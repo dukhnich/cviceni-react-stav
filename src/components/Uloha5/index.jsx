2a64aada070d5406ad393b719565999deacc1567
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-02 19:52:21 +0100

    ukol 5

--- src/components/Uloha5/index.jsx
+++ src/components/Uloha5/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import './hamburger.css';
 
 // Zadání 1: Přichystejte si stavovou proměnnou s výchozí hodnotou `false`, ve které bude uloženo, jestli je menu otevřené nebo nikoliv.
@@ -6,27 +8,32 @@ import './hamburger.css';
 // Zadání 4: Pokud je menu zavřené nechte mu jen třídu `hamburger`. Pro otevřené přidejte navíc `hamburger--otevrene`.
 
 export const Uloha5 = () => {
+  const [isMenu, setIsMenu] = useState(false);
+  const toggleMenu = () => setIsMenu(!isMenu);
   return (
     <>
-      <button className="hamburger" aria-label="menu">
+      <button className="hamburger" aria-label="menu" onClick={toggleMenu}>
         <span></span>
         <span></span>
         <span></span>
       </button>
-      <ul>
-        <li>
-          <a href="#o-nas">O nás</a>
-        </li>
-        <li>
-          <a href="#co-delame">Co děláme</a>
-        </li>
-        <li>
-          <a href="#kontakty">Kontakty</a>
-        </li>
-        <li>
-          <a href="#cenik">Ceník</a>
-        </li>
-      </ul>
+      {isMenu ? (
+        <ul>
+          <li>
+            <a href="#o-nas">O nás</a>
+          </li>
+          <li>
+            <a href="#co-delame">Co děláme</a>
+          </li>
+          <li>
+            <a href="#kontakty">Kontakty</a>
+          </li>
+          <li>
+            <a href="#cenik">Ceník</a>
+          </li>
+        </ul>
+      ) : null}
+      
     </>
   );
 };
